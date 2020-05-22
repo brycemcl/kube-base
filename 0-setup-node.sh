9fe1554f259b87871ecc420d3b080b7c48ab4660
brycemcl <33608313+brycemcl@users.noreply.github.com>
2020-05-21 19:35:37 -0700

    Update 0-setup-node.sh

--- 0-setup-node.sh
+++ 0-setup-node.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-sed -i.bak -r 's/(.+ swap .+)/#\1/' /etc/fstab
+sudo sed -i.bak -r 's/(.+ swap .+)/#\1/' /etc/fstab
 git pull
-sudo su
-apt-get update && apt-get install -y apt-transport-https curl containerd kubectl kubelet kubeadm haproxy 
-apt-mark hold kubelet kubeadm kubectl
-cp node-config/containerd.conf /etc/modules-load.d/containerd.conf
-cp node-config/k8s.conf /etc/sysctl.d/k8s.conf 
-cp node-config/haproxy.cfg /etc/haproxy/haproxy.cfg
-cp node-config/sys-fs-bpf.mount /etc/systemd/system/sys-fs-bpf.mount
-reboot
+
+sudo apt-get update && apt-get install -y apt-transport-https curl containerd kubectl kubelet kubeadm haproxy 
+sudo apt-mark hold kubelet kubeadm kubectl
+sudo cp node-config/containerd.conf /etc/modules-load.d/containerd.conf
+sudo cp node-config/k8s.conf /etc/sysctl.d/k8s.conf 
+sudo cp node-config/haproxy.cfg /etc/haproxy/haproxy.cfg
+sudo cp node-config/sys-fs-bpf.mount /etc/systemd/system/sys-fs-bpf.mount
+sudo reboot

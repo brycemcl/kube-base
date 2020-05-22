1e744f5d8da6fda4beaeaea357d5a8298f016554
brycemcl <33608313+brycemcl@users.noreply.github.com>
2020-05-21 19:41:57 -0700

    Update 0-setup-node.sh

--- 0-setup-node.sh
+++ 0-setup-node.sh
@@ -2,7 +2,8 @@
 git pull
 sudo sed -i.bak -r 's/(.+ swap .+)/#\1/' /etc/fstab
 
-sudo apt-get update && apt-get install -y apt-transport-https curl containerd kubectl kubelet kubeadm haproxy 
+sudo apt-get update 
+sudo apt-get install -y apt-transport-https curl containerd kubectl kubelet kubeadm haproxy apache2-utils
 sudo apt-mark hold kubelet kubeadm kubectl
 sudo cp node-config/containerd.conf /etc/modules-load.d/containerd.conf
 sudo cp node-config/k8s.conf /etc/sysctl.d/k8s.conf 

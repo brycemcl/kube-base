1b205f26e5add09f7584b0fc8b221b7c3f244656
Bryce McLachlan <brycemclachlan@localhost.localdomain>
2020-07-15 22:55:35 -0700

    .

--- 0-setup-node.sh
+++ 0-setup-node.sh
@@ -2,9 +2,16 @@
 git pull
 sudo sed -i.bak -r '/swap/d' /etc/fstab
 
+
+sudo apt-get update && sudo apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install -y apt-transport-https curl containerd kubectl kubelet kubeadm haproxy apache2-utils
+sudo apt-get install -y containerd kubectl kubelet kubeadm haproxy apache2-utils
 sudo apt-mark hold kubelet kubeadm kubectl
 sudo cp node-config/containerd.conf /etc/modules-load.d/containerd.conf
 sudo cp node-config/k8s.conf /etc/sysctl.d/k8s.conf 

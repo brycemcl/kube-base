a5d3fbd0f7f08548a77eecc077f793ec22a4934c
Bryce McLachlan <brycemclachlan@localhost.localdomain>
2020-07-21 16:40:25 -0700

    .

--- 0a-setup-gpu.sh
+++ 0a-setup-gpu.sh
@@ -7,4 +7,5 @@ distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
 curl -s -L https://nvidia.github.io/nvidia-container-runtime/$distribution/nvidia-container-runtime.list | sudo tee /etc/apt/sources.list.d/nvidia-container-runtime.list
 sudo apt-get update
 sudo apt-get install nvidia-container-runtime -y
+sudo mkdir /etc/containerd/
 sudo cp node-config/containerd_gpu.toml /etc/containerd/config.toml

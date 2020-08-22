392a009fe1525a9b19fdaf008c4384765e213fa6
Bryce McLachlan <brycemclachlan@localhost.localdomain>
2020-08-22 15:27:11 -0700

    .

--- 0-setup-node.sh
+++ 0-setup-node.sh
@@ -16,18 +16,10 @@ sudo cp node-config/containerd.conf /etc/modules-load.d/containerd.conf
 sudo cp node-config/k8s.conf /etc/sysctl.d/k8s.conf 
 sudo cp node-config/haproxy.cfg /etc/haproxy/haproxy.cfg
 sudo cp node-config/sys-fs-bpf.mount /etc/systemd/system/sys-fs-bpf.mount
-sudo sh -c 'echo 0 > /sys/block/bcache0/bcache/sequential_cutoff'
-sudo sh -c 'echo 0 > /sys/block/bcache1/bcache/sequential_cutoff'
-sudo sh -c 'echo 0 > /sys/block/bcache2/bcache/sequential_cutoff'
-sudo sh -c 'echo 0 > /sys/block/bcache3/bcache/sequential_cutoff'
-sudo sh -c 'echo 0 > /sys/block/bcache4/bcache/sequential_cutoff'
-sudo sh -c 'echo 0 > /sys/block/bcache5/bcache/sequential_cutoff'
+sudo fallocate -l 50G /loop0.img
+sudo chmod 600 /loop0.img
+sudo losetup /dev/loop0 /loop0.img
 
-sudo sh -c 'echo writeback > /sys/block/bcache0/bcache/cache_mode'
-sudo sh -c 'echo writeback > /sys/block/bcache1/bcache/cache_mode'
-sudo sh -c 'echo writeback > /sys/block/bcache2/bcache/cache_mode'
-sudo sh -c 'echo writeback > /sys/block/bcache3/bcache/cache_mode'
-sudo sh -c 'echo writeback > /sys/block/bcache4/bcache/cache_mode'
-sudo sh -c 'echo writeback > /sys/block/bcache5/bcache/cache_mode'
+
 
 sudo reboot
